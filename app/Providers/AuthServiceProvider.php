319650f7f76909013699f97ade846bcfa570a24d
abhi-cognitivo <abhi@cognitivo.in>
2018-04-12 06:21:25 -0400

    changes to AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,29 +2,36 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
+    * The policy mappings for the application.
+    *
+    * @var array
+    */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
+    * Register any authentication / authorization services.
+    *
+    * @return void
+    */
     public function boot()
     {
         $this->registerPolicies();
-
         //
+
+        Passport::routes();
+        Passport::enableImplicitGrant();
+        Passport::tokensCan([
+            'Create' => 'Create Transactions',
+            'Read' => 'Create Transactions',
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,10 +16,12 @@
 
                         You are logged in!
                     </div>
+                    <div class="">
+                        <passport-clients></passport-clients>
+                        <passport-authorized-clients></passport-authorized-clients>
+                        <passport-personal-access-tokens></passport-personal-access-tokens>
+                    </div>
 
-                    <passport-clients></passport-clients>
-                    <passport-authorized-clients></passport-authorized-clients>
-                    <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
             </div>
         </div>
