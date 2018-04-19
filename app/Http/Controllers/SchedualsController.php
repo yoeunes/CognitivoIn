01992313a83b6526850c93fc715f73b6e7a96386
Pankeel <pankeel@cognitivo.in>
2018-04-19 16:48:50 +0530

    update

--- app/Http/Controllers/SchedualsController.php
+++ app/Http/Controllers/SchedualsController.php
@@ -146,7 +146,7 @@ class SchedualsController extends Controller
           }
           public function ReceivePayment(Request $request,$profile)
           {
-        
+
             $data=$request;
             $scheduals=new Scheduals();
             $scheduals->relationship_id=$data['relationship_id'];
@@ -163,7 +163,7 @@ class SchedualsController extends Controller
             $scheduals->rate=$data['rate'];
             $scheduals->date=Carbon::now();
             $scheduals->date_exp=Carbon::now();
-            $scheduals->credit=$data['"amount"'];
+            $scheduals->credit=$data['amount'];
             $scheduals->save();
 
 
