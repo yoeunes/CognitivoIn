d1a655716dcffec15eab85a8fe6c88a7a6c53de4
Pankeel <pankeel@cognitivo.in>
2018-05-08 18:01:56 +0530

    update

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -117,7 +117,7 @@ class AccountController extends Controller
       $accountmovement->type_id=$data['Type'];
       // $currency=Currency::where('code',$data['CurrencyCode'])
       // ->orderBy('created_at', 'desc')->first();
-      $accountmovement->currency_id=$data['currency_code'];
+      $accountmovement->currency=$data['currency_code'];
       $accountmovement->currency_rate=1;
       $accountmovement->date=Carbon::now();
       if ($data['Type']==1)
