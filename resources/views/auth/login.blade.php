cc4aa44eccf293b20497df42d0fd9ae7b2899dba
abhi-cognitivo <abhi@cognitivo.in>
2018-05-08 09:03:53 -0400

    update registration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                         Get Inspired and Create.
                     </p>
                     <p class="font-italic text-white-op">
-                        Copyright &copy; <span class="js-year-copy">2017</span>
+                        Copyright &copy; <span class="js-year-copy"> 2018 </span>
                     </p>
                 </div>
             </div>
@@ -74,69 +74,4 @@
         </div>
     </div>
 
-
-    {{-- <div class="container">
-    <div class="row justify-content-center">
-    <div class="col-md-8">
-    <div class="card">
-    <div class="card-header">{{ __('Login') }}</div>
-
-    <div class="card-body">
-    <form method="POST" action="{{ route('login') }}">
-    @csrf
-
-    <div class="form-group row">
-    <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-    <div class="col-md-6">
-    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-    @if ($errors->has('email'))
-    <span class="invalid-feedback">
-    <strong>{{ $errors->first('email') }}</strong>
-</span>
-@endif
-</div>
-</div>
-
-<div class="form-group row">
-<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-<div class="col-md-6">
-
-@if ($errors->has('password'))
-<span class="invalid-feedback">
-<strong>{{ $errors->first('password') }}</strong>
-</span>
-@endif
-</div>
-</div>
-
-<div class="form-group row">
-<div class="col-md-6 offset-md-4">
-<div class="checkbox">
-<label>
-<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-</label>
-</div>
-</div>
-</div>
-
-<div class="form-group row mb-0">
-<div class="col-md-8 offset-md-4">
-<button type="submit" class="btn btn-primary">
-{{ __('Login') }}
-</button>
-
-<a class="btn btn-link" href="{{ route('password.request') }}">
-{{ __('Forgot Your Password?') }}
-</a>
-</div>
-</div>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div> --}}
 @endsection
