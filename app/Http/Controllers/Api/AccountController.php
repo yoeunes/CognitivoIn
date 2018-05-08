bd343bb981b9efc0e5b76262db419a52d437db76
Pankeel <pankeel@cognitivo.in>
2018-05-08 18:12:08 +0530

    update

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -40,15 +40,15 @@ class AccountController extends Controller
         $data=$request[0];
 
         if ($data['Type']==1) {
-          $relationship=Relationship::GetCustomers()
-          ->where('customer_alias',$data['PartnerName'])
-          ->orWhere('customer_taxid',$data['PartnerTaxID'])->first();
+            $relationship=Relationship::GetCustomers()
+            ->where('customer_alias',$data['PartnerName'])
+            ->orWhere('customer_taxid',$data['PartnerTaxID'])->first();
 
         }
         else {
-          $relationship=Relationship::GetSuppliers()
-          ->where('supplier_alias',$data['PartnerName'])
-          ->orWhere('supplier_taxid',$data['PartnerTaxID'])->first();
+            $relationship=Relationship::GetSuppliers()
+            ->where('supplier_alias',$data['PartnerName'])
+            ->orWhere('supplier_taxid',$data['PartnerTaxID'])->first();
         }
 
         $schedules=Scheduals::where('relationship_id',$relationship->id)
@@ -69,80 +69,98 @@ class AccountController extends Controller
         $values = [];
         for ($i=0; $i <count($schedules) ; $i++)
         {
-          $values[$i] = [
-            'CurrencyCode' => $schedules[$i]->code ,
-            'Value' => $schedules[$i]->value ,
-            'MaxValue' => $schedules[$i]->max_value,
-            'ReferenceCode' => $schedules[$i]->reference,
-            'InvoiceNumber' => $schedules[$i]->InvoiceNumber,
-            'InvoiceDate' => $schedules[$i]->InvoiceDate,
-            'Deadline' => $schedules[$i]->Deadline,
-          ];
+            $values[$i] = [
+                'CurrencyCode' => $schedules[$i]->code ,
+                'Value' => $schedules[$i]->value ,
+                'MaxValue' => $schedules[$i]->max_value,
+                'ReferenceCode' => $schedules[$i]->reference,
+                'InvoiceNumber' => $schedules[$i]->InvoiceNumber,
+                'InvoiceDate' => $schedules[$i]->InvoiceDate,
+                'Deadline' => $schedules[$i]->Deadline,
+            ];
         }
 
         $data2[] = [
-          'ReferenceName' => $data['PartnerName'],
-          'ReferenceTaxID' => $data['PartnerTaxID'],
-          'Details' => $values ];
+            'ReferenceName' => $data['PartnerName'],
+            'ReferenceTaxID' => $data['PartnerTaxID'],
+            'Details' => $values ];
 
 
-          return response()->json($data2,'200');
+            return response()->json($data2,'200');
+        }
+
+        public function recievePayment(Request $request, Profile $profile)
+        {
+
+            //Store payment information recieved by client application
+            $data=$request[0];
+            if (!isset($data)) {
+                $data=$request;
+            }
+
+            if ($data['Type']==1) {
+                $relationship=Relationship::GetCustomers()
+                ->where('customer_alias',$data['PartnerName'])
+                ->orWhere('customer_taxid',$data['PartnerTaxID'])->first();
+
+            }
+            else {
+                $relationship=Relationship::GetSuppliers()
+                ->where('supplier_alias',$data['PartnerName'])
+                ->orWhere('supplier_taxid',$data['PartnerTaxID'])->first();
+            }
+
+            $schedual= new Scheduals();
+            $schedual->relationship_id=$relationship->id;
+            $schedual->currency='PRY';
+            $schedual->currency_rate=1;
+            $schedual->date=Carbon::now();
+            $schedual->date_exp=Carbon::now();
+            if ($data['Type']==1)
+            {
+                $schedual->credit=$data['total_amount'];
+                $schedual->debit=0;
+            }
+            else
+            {
+                $schedual->credit=0;
+                $schedual->debit=$data['total_amount'];
+            }
+            $schedual->save();
+
+            $accountmovement=new AccountMovement();
+            $accountmovement->schedual_id=$schedual->id;
+            $accountmovement->user_id=$relationship->id;
+            $accountmovement->account_id=$data['AccountID'];
+            $accountmovement->type_id=$data['Type'];
+            // $currency=Currency::where('code',$data['CurrencyCode'])
+            // ->orderBy('created_at', 'desc')->first();
+            $accountmovement->currency='PRY';
+            $accountmovement->currency_rate=1;
+            $accountmovement->date=Carbon::now();
+            if ($data['Type']==1)
+            {
+                $accountmovement->credit=$data['Value'];
+                $accountmovement->debit=0;
+            }
+            else
+            {
+                $accountmovement->credit=0;
+                $accountmovement->debit=$data['Value'];
+            }
+            $accountmovement->save();
+
+            $data2 = [];
+
+            $data2[] = [
+                'PaymentReference' => $accountmovement->id,
+                'ResponseType' => 1
+            ];
+            return response()->json($data2,'200');
+        }
+
+        public function annullPayment(Request $request, Profile $profile)
+        {
+            //in request, take paymentID given by previous payment to annull value
+        }
     }
-
-    public function recievePayment(Request $request, Profile $profile)
-    {
-        //Store payment information recieved by client application
-        $data=$request[0];
-      if (!isset($data)) {
-        $data=$request;
-      }
-
-      if ($data['Type']==1) {
-        $relationship=Relationship::GetCustomers()
-        ->where('customer_alias',$data['PartnerName'])
-        ->orWhere('customer_taxid',$data['PartnerTaxID'])->first();
-
-      }
-      else {
-        $relationship=Relationship::GetSuppliers()
-        ->where('supplier_alias',$data['PartnerName'])
-        ->orWhere('supplier_taxid',$data['PartnerTaxID'])->first();
-      }
-
-
-      $accountmovement=new AccountMovement();
-      $accountmovement->schedual_id=$data['InvoiceReference'];
-      $accountmovement->user_id=$relationship->id;
-      $accountmovement->account_id=$data['AccountID'];
-      $accountmovement->type_id=$data['Type'];
-      // $currency=Currency::where('code',$data['CurrencyCode'])
-      // ->orderBy('created_at', 'desc')->first();
-      $accountmovement->currency='PRY';
-      $accountmovement->currency_rate=1;
-      $accountmovement->date=Carbon::now();
-      if ($data['Type']==1)
-      {
-        $accountmovement->credit=$data['Value'];
-        $accountmovement->debit=0;
-      }
-      else
-      {
-        $accountmovement->credit=0;
-        $accountmovement->debit=$data['Value'];
-      }
-      $accountmovement->save();
-
-      $data2 = [];
-
-      $data2[] = [
-        'PaymentReference' => $accountmovement->id,
-        'ResponseType' => 1
-      ];
-      return response()->json($data2,'200');
-    }
-
-    public function annullPayment(Request $request, Profile $profile)
-    {
-        //in request, take paymentID given by previous payment to annull value
-    }
-}
