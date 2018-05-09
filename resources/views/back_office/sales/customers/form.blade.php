14a0201e3355b12b0f9b043775f010bdde0d9138
Pankeel <pankeel@cognitivo.in>
2018-05-09 18:38:12 +0530

    update

--- resources/views/back_office/sales/customers/form.blade.php
+++ resources/views/back_office/sales/customers/form.blade.php
@@ -1,4 +1,4 @@
-<customer-form profile="{{ request()->route('profile') }}" inline-template>
+<customer-form profile="{{ request()->route('profile')->slug }}" inline-template>
   <div>
     <div class="row">
       <div class="form-group">
