43cbe2dacedb2c9b9890b19688ea7cb5bbf6f118
Pankeel <pankeel@cognitivo.in>
2018-05-10 11:51:33 +0530

    update

--- app/Http/Controllers/Api/RelationshipController.php
+++ app/Http/Controllers/Api/RelationshipController.php
@@ -93,7 +93,7 @@ class RelationshipController extends Controller
 
     public function customers(Profile $profile,Request $request)
      {
-        
+         return response()->json($request[0],500);
        $relationship= new Relationship();
        $relationship->supplier_id = $profile->id;
        $relationship->customer_taxid=$request[0]['taxid'];
