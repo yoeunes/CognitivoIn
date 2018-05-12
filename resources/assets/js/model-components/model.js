b34a38dc9695fb9ec9a661ab5185daf378e016d8
abhi-cognitivo <abhi@cognitivo.in>
2018-05-12 15:33:07 -0400

    update sweet alert functions to help with user interface

--- resources/assets/js/model-components/model.js
+++ resources/assets/js/model-components/model.js
@@ -81,19 +81,33 @@ Vue.component('model',
         onEdit($url, $data)
         {
             var app = this;
-            app.showList = false;
 
             axios.get('/api/' + this.profile + '/back-office/list/'  + app.skip + '/' + app.url + '/' + app.filterListBy)
             .then(({ $data }) =>
             {
+                app.showList = false;
                 return $data[0];
+            })
+            .catch(error => {
+                console.log(error.response.data);
+                this.$swal('Error trying to edit record.');
             });
         },
 
         onCancel($data)
         {
             var app = this;
-
+            swal({
+                title: 'Are you sure?',
+                text: "You will loose all your data",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, cancel it!'
+            }).then((result) => {
+                app.showList = true;
+            })
         }
 
         onSave($url, $data)
@@ -102,43 +116,108 @@ Vue.component('model',
             axios.put('/api/cruds/${id}', { color })
             .then(() =>
             {
-                // Once AJAX resolves we can update the Crud with the new color
-                this.cruds.find(crud => crud.id === id).color = color;
-            });
+                app.showList = true;
+                this.$swal({
+                    position: 'top-end',
+                    type: 'success',
+                    title: 'Your work has been saved',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            })
+            .catch({
 
+            });
         },
 
         onSaveCreate($url, $data)
         {
             var app = this;
 
+            axios.put('/api/cruds/${id}', { color })
+            .then(() =>
+            {
+                //TODO run code to clean data.
+                app.showList = false;
+                this.$swal({
+                    position: 'top-end',
+                    type: 'success',
+                    title: 'Your work has been saved',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            })
+            .catch({
+
+            });
         },
 
         onDelete($url, $data)
         {
             var app = this;
 
-            axios.delete('/api/' + this.profile + '/back-office/' + app.url, {
-                params: { $data: this.data.ID }
+            swal({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                type: 'danger',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                axios.delete('/api/' + this.profile + '/back-office/' + app.url, {
+                    params: { $data: this.data.ID }
+                })
+                .then(() => {
+                    let index = this.list.findIndex(crud => list.ID === ID);
+                    this.list.splice(index, 1);
+
+                    this.$swal({
+                        position: 'top-end',
+                        type: 'success',
+                        title: 'The record has been deleted',
+                        showConfirmButton: false,
+                        timer: 750
+                    })
+                })
+                .catch({
+
+                });
             })
-            .then(() => {
-                let index = this.list.findIndex(crud => list.ID === ID);
-                this.list.splice(index, 1);
-
-
-            });
         },
 
         onApprove($url, $data)
         {
             var app = this;
 
+            swal({
+                title: 'You are about to Approve',
+                text: "This will process your record",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, approve it!'
+            }).then((result) => {
+                //Code to approve
+            })
         },
 
         onAnnull($url, $data)
         {
             var app = this;
 
+            swal({
+                title: 'Annull? Are you sure?',
+                text: "This is non-reversable",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, annull it!'
+            }).then((result) => {
+                //Code to annull
+            })
         }
     },
 
