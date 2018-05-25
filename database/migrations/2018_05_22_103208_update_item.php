534fb77e5dacf34485000b4c16de419a32d4aa5e
Pankeel <pankeel@cognitivo.in>
2018-05-25 14:31:38 +0530

    update

---
+++ database/migrations/2018_05_22_103208_update_item.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateItems extends Migration
+{
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::table('items', function (Blueprint $table)
+    {
+      $table->boolean('is_stockable')->default(false);
+    });
+
+
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+    Schema::table('items', function (Blueprint $table)
+    {
+
+
+      $table->dropColumn(['is_stockable']);
+
+    });
+
+
+  }
+}
