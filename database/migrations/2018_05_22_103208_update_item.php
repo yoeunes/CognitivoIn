adbf89672add54873cea8c563d2afe7bd0513243
Pankeel <pankeel@cognitivo.in>
2018-05-25 14:37:23 +0530

    update

--- database/migrations/2018_05_22_103208_update_item.php
+++ database/migrations/2018_05_22_103208_update_item.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateItems extends Migration
+class UpdateItem extends Migration
 {
   /**
   * Run the migrations.
