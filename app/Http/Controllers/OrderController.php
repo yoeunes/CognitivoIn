69892ffbfc2a5e56389a5f29cde97afc18691a72
abhi-cognitivo <abhi@cognitivo.in>
2018-07-19 15:21:46 -0400

    updates to logic. database migration will be required for approval and annull functions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -59,9 +59,14 @@ class OrderController extends Controller
     {
         if (count($request->details) > 0)
         {
-            $order = Order::where('id', $request->cloud_id)->with('details')->first() ?? new Order();
+            $order = Order::mySales()
+            ->where('id', $request->cloud_id)
+            ->with('details')
+            ->first()
+            ??
+            new Order();
 
-            $order->relationship_id = $request->relationship_id;
+            $order->relationship_id = $request->relationship_cloud_id;
             $order->currency = $request->currency ?? $profile->currency;
             $order->currency_rate = ($request->rate ?? Swap::latest($profile->currency . '/' . $request->currency)->getValue()) ?? 1;
             $order->is_impex = $request->is_impex ?? 0;
@@ -72,7 +77,7 @@ class OrderController extends Controller
 
             foreach ($request->details as $detail)
             {
-                $orderDetail = $order->details->where('id', $detail['cloud_id'])->first() ?? new OrderDetail();
+                $orderDetail = $order->details->where('id', $detail['detail_cloud_id'])->first() ?? new OrderDetail();
                 $orderDetail->order_id = $order->id;
                 $orderDetail->item_id = $detail['item_id'];
                 $orderDetail->item_sku = $detail['sku'];
@@ -110,10 +115,10 @@ class OrderController extends Controller
             //     }
             // }
 
-            return response()->json('success', 200);
+            return response()->json('Ok', 200);
         }
 
-        return response()->json('fail', 500);
+        return response()->json('Resource not found', 403);
     }
 
     /**
@@ -124,12 +129,16 @@ class OrderController extends Controller
     */
     public function show(Profile $profile, $orderID)
     {
-        $order = Order::where('id', $orderID)
+        $order = Order::mySales()
+        ->where('id', $orderID)
         ->with('details')
-        ->with('relationship')
         ->first();
 
-        return response()->json($order);
+        if (isset($order))
+        { return response()->json($order); }
+        else
+        { return response()->json('Forbidden', 403); }
+
     }
 
     /**
@@ -140,12 +149,15 @@ class OrderController extends Controller
     */
     public function edit(Profile $profile, Order $order)
     {
-        $order = Order::where('orders.id', $order->id)
+        $order = Order::mySales()
+        ->where('orders.id', $order->id)
         ->with('details')
-        ->with('relationship')
         ->first();
 
-        return response()->json($order);
+        if (isset($order))
+        { return response()->json($order); }
+        else
+        { return response()->json('Forbidden', 403); }
     }
 
     public function destroy(Order $order)
@@ -153,21 +165,26 @@ class OrderController extends Controller
         //
     }
 
-    public function approve($profile, $orderID)
+    public function approve(Profile $profile, $orderID)
     {
-        $order = Order::where('id', $orderID)->with('details')->first();
+        $order = Order::mySales()
+        ->where('id', $orderID)
+        ->with('details')
+        ->first();
 
-        $amount = 0;
-        $vatAmount = 0;
+        if (isset($order))
+        {
+            return response()->json('Resource not found', 404);
+        }
 
         if ($order->status == 2)
         {
-            //If status 2, then you cannot approve as it is already approved.
-            return response()->json('Forbidden', 403);
+            //If status 3, then you cannot annull as it is already annulled.
+            return response()->json('Forbidden. Order is already approved', 403);
         }
 
-        //for stock entry
-        $this->stockEntry($order);
+        $amount = 0;
+        $vatAmount = 0;
 
         //for payment entry
         if ($order->contract_id > 0)
@@ -182,9 +199,9 @@ class OrderController extends Controller
                 $schedule->relationship_id = $order->relationship_id;
                 $schedule->reference = $order->number;
                 $schedule->currency = $order->currency;
-                $schedule->currency_rate = $order->currency_rate;
-                $schedule->date = Carbon::now();
-                $schedule->due_date = Carbon::now()->addDays($detail->offset);
+                $schedule->currency_rate = $order->currency_rate ?? 1;
+                $schedule->date = $order->date ?? Carbon::now();
+                $schedule->due_date = $schedule->date->addDays($detail->offset);
                 $schedule->value = $amount * $detail->percent;
                 $schedule->save();
 
@@ -210,65 +227,83 @@ class OrderController extends Controller
             $schedule->save();
         }
     }
-
-    public function stockEntry($order)
-    {
-        foreach ($order->details as  $detail)
-        {
-            $vatDetails = VatDetail::where('vat_id', $detail->vat_id)->get();
-
-            foreach ($vatDetails as $vat)
-            {
-                $vatAmount = $vatAmount + (($detail->unit_price * $vat->percent) *  $vat->coefficient);
-            }
-
-            $amount = $amount + (($detail->unit_price + $vatAmount) * $detail->quantity);
-
-            //TODO Get only required column. you are using one column only.
-            $item = Item::find($detail->item_id);
-
-            if ($item->is_stockable && $order->location_id != null)
-            {
-                $movement = new ItemMovement();
-                $movement->item_id = $item->id;
-                $movement->order_id = $order->id;
-                $movement->location_id = $order->location_id;
-                $movement->date = Carbon::now();
-                $movement->credit = 0;
-                $movement->debit = $detail->quantity;
-                $movement->save();
-            }
-        }
-    }
+    //
+    // public function stockEntry($order)
+    // {
+    //     foreach ($order->details as  $detail)
+    //     {
+    //         $vatDetails = VatDetail::where('vat_id', $detail->vat_id)->get();
+    //
+    //         foreach ($vatDetails as $vat)
+    //         {
+    //             $vatAmount = $vatAmount + (($detail->unit_price * $vat->percent) *  $vat->coefficient);
+    //         }
+    //
+    //         $amount = $amount + (($detail->unit_price + $vatAmount) * $detail->quantity);
+    //
+    //         //TODO Get only required column. you are using one column only.
+    //         $item = Item::find($detail->item_id);
+    //
+    //         if ($item->is_stockable && $order->location_id != null)
+    //         {
+    //             $movement = new ItemMovement();
+    //             $movement->item_id = $item->id;
+    //             $movement->order_id = $order->id;
+    //             $movement->location_id = $order->location_id;
+    //             $movement->date = Carbon::now();
+    //             $movement->credit = 0;
+    //             $movement->debit = $detail->quantity;
+    //             $movement->save();
+    //         }
+    //     }
+    // }
 
     //TODO make annull code for order.
-    public function annul($orderID)
+    public function annul(Profile $profile, $orderID)
     {
-        $order = Order::where('id', $orderID)->with('detail')->first();
+        $order = Order::mySales()
+        ->where('id', $orderID)
+        ->with('detail')
+        ->first();
+
+        if (isset($order))
+        {
+            return response()->json('Resource not found', 404);
+        }
 
         if ($order->status == 3)
         {
-            //If status 2, then you cannot annull as it is already annulled.
-            return response()->json('Forbidden', 403);
+            //If status 3, then you cannot annull as it is already annulled.
+            return response()->json('Forbidden. Order is already annulled', 403);
         }
 
         $amount = 0;
         $vatAmount = 0;
 
-        //for stock entry
-        foreach ($order->detail as  $detail)
-        {
-            //TODO Get only required column. you are using one column only.
-            $item = Item::find($detail->item_id);
+        //Get list of stock movements generated from
+        $movements = ItemMovement::whereIn('id', [$order->details->movement_id])->get();
 
-            if ($item->is_stockable && $order->location_id != null)
+        foreach ($movements as $movement)
+        {
+            $details = $order->details->where('movement_id', $movement->id)->get();
+
+            //check if movement quantity = detail quantity
+            if ($movement->debit == $details->sum('quantity'))
             {
-                $movement = ItemMovement::where('order_id', $order->id)->first();
+                //if movement equals to detail quantity, then delete.
                 $movement->delete();
+            }
+            else
+            {
+                //if not, other transactionsa are included. so discount quantity but keep record.
+                $movement->debit = $movement->debit - $details->sum('quantity');
+                $movement->save();
             }
         }
 
         //for payment entry
+        $schedule = new Schedule();
+
         if ($order->contract_id > 0)
         {
             $contractDetails = ContractDetail::where('contract_id', $order->contract_id)->get();
@@ -293,18 +328,6 @@ class OrderController extends Controller
             {
                 // add difference to last schedual on list.
             }
-        }
-        else
-        {
-            $schedule = new Schedule();
-            $schedule->relationship_id = $order->relationship_id;
-            $schedule->currency = $order->currency;
-            $schedule->currency_rate = $order->currency_rate;
-            $schedule->date = Carbon::now();
-            $schedule->date_exp = Carbon::now();
-            $schedule->credit = $amount;
-            $schedule->debit = 0;
-            $schedule->save();
         }
     }
 
