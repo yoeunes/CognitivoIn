e4a1bab58237201f43b02df671700b2a0375827f
pankeel <pankeel@cognitivo.in>
2018-07-25 17:51:33 +0530

    r

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Profile;
+use App\VatDetail;
 use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\AccountMovementController;
