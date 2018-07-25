758bb649dc46a8717b78c4ba6a471c6e2b63a94c
pankeel <pankeel@cognitivo.in>
2018-07-25 17:15:42 +0530

    update

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -35,10 +35,10 @@ class TransactionController extends Controller
         }
 
         $collection = json_decode($data->toJson());
-
+    return response()->json($collection, 500);
         foreach ($collection as $key => $data)
         {
-            return response()->json($data, 500);
+
 
             $orderController = new OrderController();
 
