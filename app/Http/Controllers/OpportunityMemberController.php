4c8cee4edf336b2fcde9a53b2cb43df706d680f1
pankeel <pankeel@cognitivo.in>
2018-07-25 19:00:19 +0530

    update

--- app/Http/Controllers/OpportunityMemberController.php
+++ app/Http/Controllers/OpportunityMemberController.php
@@ -15,7 +15,8 @@ class OpportunityMemberController extends Controller
   */
   public function index(Profile $profile, $skip)
   {
-    $opportunityMembers = OpportunityMember::My()
+    $opportunityMembers = OpportunityMember::where('profile_id',$request->profile_id)
+    ->where('opportunity_id',$opportunity->id)
     ->skip($skip)
     ->take(25)
     ->get();
