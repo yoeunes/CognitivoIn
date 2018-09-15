a476cf31da31e9dd722f8f0756ed46dfa74ff6ea
abhi-cognitivo <abhi@cognitivo.in>
2018-09-15 11:20:11 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -12,35 +12,31 @@
 */
 
 Auth::routes();
-Route::get('/settings','SettingController@index')->name('settings');
-Route::get('/{profile?}', 'HomeController@index')->name('home');
+
 Route::get('/market', 'HomeController@indexMarket')->name('market.index');
 Route::get('/shop/{profile}', 'HomeController@indexStores')->name('shop.show');
 Route::get('/shop/{profile}/item/{item}', 'ItemController@indexStores')->name('shop.item.show');
 
 Route::group(['middleware' => 'auth'], function ()
 {
-  Route::resources([
-    'inbox' => 'MessageController',
-    'profile' => 'ProfileController'
-  ]);
+    Route::get('/settings','SettingController@index')->name('settings');
+    Route::get('/{profile?}', 'HomeController@index')->name('home');
 
+    Route::resources([
+        'inbox' => 'MessageController',
+        'profile' => 'ProfileController'
+    ]);
 
+    Route::prefix('/dashboard/{profile}')->group(function ()
+    {
+        Route::get('/{url}', 'NavigationController@index')->where('any', '.*');
+    });
 
-  Route::prefix('/dashboard/{profile}')->group(function ()
-  {
-    Route::get('/{url}', 'NavigationController@index')->where('any', '.*');
-  });
+    Route::prefix('reports')->group(function ()
+    {
+        Route::get('/index', 'ReportController@index')->name('reports.index');
 
-
-  Route::prefix('reports')->group(function ()
-  {
-    Route::get('/index', 'ReportController@index')->name('reports.index');
-
-
-    Route::get('opportunities/{profile}/{strDate}/{endDate}', 'ReportController@opportunity')->name('reports.opportunities');
-    Route::get('opportunitiesTask/{profile}/{strDate}/{endDate}', 'ReportController@opportunityTask')->name('reports.opportunitiesTask');
-  });
-
-  Route::impersonate();
+        Route::get('opportunities/{profile}/{strDate}/{endDate}', 'ReportController@opportunity')->name('reports.opportunities');
+        Route::get('opportunitiesTask/{profile}/{strDate}/{endDate}', 'ReportController@opportunityTask')->name('reports.opportunitiesTask');
+    });
 });
