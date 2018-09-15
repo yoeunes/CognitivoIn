c1ba1c384e10beea1668443aa2e1766e5fff59ff
abhi-cognitivo <abhi@cognitivo.in>
2018-09-15 10:43:36 -0400

    Update Nova Code

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ],
 
